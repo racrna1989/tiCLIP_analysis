af61e12e959e4c21812463ea42ff3873dcf075e6
racrna1989 <107215002+racrna1989@users.noreply.github.com>
2022-12-03 15:27:23 +0000

    Delete master_scripts directory

--- master_scripts/1_raw_to_demultiplexed_UMItoHeader_trimGalore/1_2_4_rename.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-#rename files to have read number before prefix of .fastq. This is required for trim_galore
-
-WD="/home/racrna/4_xiCLIP/2_demultiplexing/3_RT_L3_demultiplexed/"
-cd $WD
-for f in *R1.processed*; do rename .fastq _1.fastq $f; done
-for f in *R2.processed*; do rename .fastq _2.fastq $f; done
